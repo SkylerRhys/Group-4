595137d514ee14b5852a17818f67d3343a3099ba
Skyler Jones <skylerjones@Skylers-MacBook-Air.local>
2024-04-11 18:52:15 -0500

    basic fetch works

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,8 +1,8 @@
-const rootEl = document.getElementById('data');
+const rootEl = document.getElementById('comicData');
 console.log(rootEl);
 function getMarvelApi() {
 
-    const marvelApiStart = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=spider&apikey=";
+    const marvelApiStart = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=thanos&orderBy=modified&apikey=";
     const marvelPublicKey = '7493e7241069db22273aa9163a8086a6';
     const marvelPrivateKey = '0d7504ac031939ac69865b2724e7c563a6dcadc4';
     const name = 'thor';
@@ -17,11 +17,23 @@ function getMarvelApi() {
     })
     .then(function (data) {
         console.log(data);
+
+        const characterResults = data.data.results;
+        console.log(characterResults);
         
-        for (const character of data) {
+        for (const character of characterResults) {
+            const comicCard = document.createElement('div');
+            comicCard.setAttribute('class', 'card');
+
             const imgTag = document.createElement('img');
-            imgTag.setAttribute('src', `${character.data.results.thumbnail.path}.jpg`);
-            rootEl.append(imgTag);
+            imgTag.setAttribute('src', `${character.thumbnail.path}.${character.thumbnail.extension}`);
+            imgTag.setAttribute('class', 'comicImage');
+
+            const characterName = document.createElement('p');
+            characterName.textContent = character.name;
+            rootEl.append(comicCard);
+            comicCard.append(imgTag);
+            comicCard.append(characterName);
         }
     })
 
